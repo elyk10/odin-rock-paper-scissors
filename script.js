8ac4d22852f929f6ac1e20236ea04e55f03b48d1
elyk10 <144371327+elyk10@users.noreply.github.com>
2025-08-14 09:31:48 -0400

    working selection function
    selection function for the computers choice in rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello world");
+
+function getComputerChoice(choice)
+{
+    var randomNum = random(3);
+    randomNum = choice
+    var selection;
+
+    if (randomNum < 1)
+        selection = "rock";
+    else if (randomNum < 2)
+        selection = "paper";
+    else
+        selection = "scissors";
+
+    return selection;
+}
+
+function random(range)
+{
+    return Math.random * range;
+}
+
+console.log(getComputerChoice(0));
+console.log(getComputerChoice(1));
+console.log(getComputerChoice(2));
